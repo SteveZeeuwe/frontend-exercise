86ce72f058a666f39ada8eadbba3519b5ffdd15f
Steve Zeeuwe <szeeuwe@gmail.com>
2018-12-16 17:24:53 +0100

    Now creating proper checkbox/label list items.

--- src/js/createList.js
+++ src/js/createList.js
@@ -10,10 +10,17 @@ function renderList(){
 };
 
 function createListitem(item){
-    const listItem = document.createElement('li');
-    listItem.innerHTML = item;
+    const listEl = document.createElement('li');
 
-    return listItem;
+    const labelEl = document.createElement('label');
+    listEl.appendChild(labelEl);
+
+    const checkboxEl = document.createElement('input');
+    checkboxEl.type = 'checkbox';
+    labelEl.appendChild(checkboxEl);
+    labelEl.innerHTML += item;
+
+    return listEl;
 }
 
 export default renderList;
