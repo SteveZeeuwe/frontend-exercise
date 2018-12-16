ace116af71334d5f5304cd82633b9dc86d296a53
Steve Zeeuwe <szeeuwe@gmail.com>
2018-12-16 23:36:30 +0100

    Added a defensive check for the localstorage variable

--- src/js/useLocalStorage.js
+++ src/js/useLocalStorage.js
@@ -2,7 +2,11 @@ import {listAll} from './elementReferences.js';
 
 function useLocalStorage(){
     
-    const storedListSelection = JSON.parse(localStorage.getItem('storedListSelection'));
+    let storedListSelection = localStorage.getItem('storedListSelection')
+
+    if (storedListSelection === null) return;
+
+    storedListSelection = JSON.parse(storedListSelection);
 
     storedListSelection.forEach(element => {
         listAll.querySelector(`[data-index="${element}"] input`).checked = true;
